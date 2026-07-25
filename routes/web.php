a7a890f085ebce3590a97ab1f900ec24d5581416
suhib <suhibmanasra@gmail.com>
2026-07-26 02:52:21 +0300

    fix the erorr

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,11 @@ use App\Http\Controllers\ProductController;
 
 
 
-Route::Controller(ProductController::class)->group(function () {
+Route::controller(ProductController::class)->group(function () {
     Route::get('/products', 'index')->name('products.index');
-    Route::get('/products/{id}', 'show')->name('products.show');
+    Route::get('/products/{id}','show')->name('products.show');
 });
-Route::Controller(HomeController::class)->group(function () {
+Route::controller(HomeController::class)->group(function () {
     Route::get('/', 'index')->name('home.index');
-    Route::get('/about', 'about')->name('about.about');
+    Route::get('/about', 'about')->name('home.about');
 });
